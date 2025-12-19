ee535dba1b58ba3cb9004c26004a76d4cc19414e
Yuga Deckhand <yugalabs@users.noreply.github.com>
2025-12-19 16:04:56 +0000

    Publish version v2.0.15-rc.1

--- src/components/widget/WalletNFTsTab.tsx
+++ src/components/widget/WalletNFTsTab.tsx
@@ -4,7 +4,6 @@ import ExpandItemIcon from "../../assets/svg/ExpandItemIcon";
 import LinkedNFTChip from "../../assets/svg/LinkedNFTChip";
 import { NFTImgFail } from "../../assets/svg/NFTImgFail";
 import { useGlyph } from "../../hooks/useGlyph";
-import { formatCurrency } from "../../lib/intl";
 import { cn } from "../../lib/utils";
 import { buttonVariants } from "../ui/button";
 
@@ -39,7 +38,7 @@ export function WalletNFTsTab() {
     const { glyphUrl, balances, hasBalances } = useGlyph();
     const [expandedItemId, setExpandedItemId] = useState<string>("");
     const nfts = balances?.nfts || [];
-    const totalValue = balances?.wallet_value?.nfts;
+    // const totalValue = balances?.wallet_value?.nfts;
 
     const nftsURL = new URL("/nfts", glyphUrl).toString();
 
@@ -48,13 +47,13 @@ export function WalletNFTsTab() {
             {/* Fixed header */}
             <div className="gw-flex gw-mb-2 gw-flex-row gw-justify-between gw-items-center gw-w-full gw-flex-shrink-0 gw-pr-4">
                 <h6>My NFTs</h6>
-                {totalValue ? (
+                {/* {totalValue ? (
                     <div className="gw-typography-body2">
                         <span className="amount">
                             {formatCurrency(totalValue, balances?.wallet_value?.currency, nfts.length > 0)}
                         </span>
                     </div>
-                ) : null}
+                ) : null} */}
             </div>
 
             {hasBalances && nfts?.length ? (
